2f9806124b910f9c41ab10171b016ce0cc63efbb
admin <kieubaongoc.haui@gmail.com>
2022-11-04 02:39:49 +0700

    fix google one-tap login

--- app/src/main/java/com/ngockieubao/orderapp/ui/WelcomeFragment.kt
+++ app/src/main/java/com/ngockieubao/orderapp/ui/WelcomeFragment.kt
@@ -1,28 +1,14 @@
 package com.ngockieubao.orderapp.ui
 
-import android.content.IntentSender
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.google.android.gms.auth.api.identity.BeginSignInRequest
-import com.google.android.gms.auth.api.identity.Identity
-import com.google.android.gms.auth.api.identity.SignInClient
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.CommonStatusCodes
-import com.google.android.material.snackbar.Snackbar
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.ngockieubao.orderapp.BuildConfig
 import com.ngockieubao.orderapp.R
 import com.ngockieubao.orderapp.base.LoginViewModelFactory
 import com.ngockieubao.orderapp.databinding.FragmentWelcomeBinding
@@ -31,111 +17,27 @@ import com.ngockieubao.orderapp.ui.login.LoginViewModel
 class WelcomeFragment : Fragment() {
 
     private var _binding: FragmentWelcomeBinding? = null
-    private val binding
-        get() = _binding!!
-
-    private lateinit var auth: FirebaseAuth
-
+    private val binding get() = _binding!!
     private val loginViewModel: LoginViewModel by activityViewModels {
         LoginViewModelFactory(requireActivity().application)
     }
-
-    private var oneTapClient: SignInClient? = null
-    private var signUpRequest: BeginSignInRequest? = null
-    private var signInRequest: BeginSignInRequest? = null
-
     private val oneTapResult = registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result ->
-        try {
-            val credential = oneTapClient?.getSignInCredentialFromIntent(result.data)
-            val idToken = credential?.googleIdToken
-
-            when {
-                idToken != null -> {
-                    // Got an ID token from Google. Use it to authenticate with your backend.
-
-                    val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
-                    auth.signInWithCredential(firebaseCredential)
-                        .addOnCompleteListener(requireActivity()) { task ->
-                            if (task.isSuccessful) {
-                                // Sign in success, update UI with the signed-in user's information
-                                val user = auth.currentUser
-                                val msg = "user - ${user?.email}"
-                                Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
-                                loginViewModel.addUserData()
-                                this.findNavController().navigate(R.id.action_welcomeFragment_to_mainActivity)
-//                                updateUI(user)
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Log.w(TAG, "signInWithCredential: failure", task.exception)
-//                                updateUI(null)
-                            }
-                        }
-                }
-                else -> {
-                    // Shouldn't happen.
-                    Log.d("one tap", "No ID token!")
-                    Snackbar.make(binding.root, "No ID token!", Snackbar.LENGTH_SHORT).show()
-                }
-            }
-        } catch (e: ApiException) {
-            when (e.statusCode) {
-                CommonStatusCodes.CANCELED -> {
-                    Log.d("one tap", "One-tap dialog was closed.")
-                    // Don't re-prompt the user.
-                    Snackbar.make(binding.root, "One-tap dialog was closed.", Snackbar.LENGTH_SHORT).show()
-                }
-                CommonStatusCodes.NETWORK_ERROR -> {
-                    Log.d("one tap", "One-tap encountered a network error.")
-                    // Try again or just ignore.
-                    Snackbar.make(binding.root, "One-tap encountered a network error.", Snackbar.LENGTH_SHORT).show()
-                }
-                else -> {
-                    Log.d(
-                        "one tap", "Couldn't get credential from result." +
-                                " (${e.localizedMessage})"
-                    )
-                    Snackbar.make(
-                        binding.root, "Couldn't get credential from result.\" +\n" +
-                                " (${e.localizedMessage})", Snackbar.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }
+        loginViewModel.initOneTapResult(result, requireActivity())
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        oneTapClient = Identity.getSignInClient(requireActivity())
-        signUpRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(false)
-                    .build()
-            )
-            .build()
-        signInRequest = BeginSignInRequest.builder()
-            .setGoogleIdTokenRequestOptions(
-                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-                    .setSupported(true)
-                    // Your server's client ID, not your Android client ID.
-                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
-                    // Show all accounts on the device.
-                    .setFilterByAuthorizedAccounts(true)
-                    .build()
-            )
-            .build()
+        checkCurrentUser()
+        loginViewModel.initParams(requireActivity())
+    }
 
-        auth = Firebase.auth
-        val currentUser = auth.currentUser
+    private fun checkCurrentUser() {
+        val currentUser = loginViewModel.checkCurrentUser()
         if (currentUser == null) return
         else {
             this.findNavController().navigate(R.id.action_welcomeFragment_to_mainActivity)
-            Toast.makeText(requireActivity(), "${currentUser.email} signed in", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireActivity(), "${currentUser.email}", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -154,48 +56,24 @@ class WelcomeFragment : Fragment() {
         binding.btnLogin.setOnClickListener {
             this.findNavController().navigate(R.id.loginFragment)
         }
-
         binding.btnSignUp.setOnClickListener {
             this.findNavController().navigate(R.id.signUpFragment)
         }
-
         binding.btnOneTap.setOnClickListener {
-            displaySignIn()
+            loginViewModel.displaySignIn(
+                requireActivity(),
+                loginViewModel.oneTapClient,
+                loginViewModel.signInRequest,
+                loginViewModel.signUpRequest,
+                oneTapResult
+            )
         }
-    }
 
-    private fun displaySignIn() {
-        oneTapClient?.beginSignIn(signInRequest!!)
-            ?.addOnSuccessListener(requireActivity()) { result ->
-                try {
-                    val ib = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
-                    oneTapResult.launch(ib)
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e("btn click", "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            ?.addOnFailureListener(requireActivity()) { e ->
-                // No Google Accounts found. Just continue presenting the signed-out UI.
-                displaySignUp()
-                Log.d("btn click", e.localizedMessage!!)
-            }
-    }
-
-    private fun displaySignUp() {
-        oneTapClient?.beginSignIn(signUpRequest!!)
-            ?.addOnSuccessListener(requireActivity()) { result ->
-                try {
-                    val ib = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
-                    oneTapResult.launch(ib)
-                } catch (e: IntentSender.SendIntentException) {
-                    Log.e("btn click", "Couldn't start One Tap UI: ${e.localizedMessage}")
-                }
-            }
-            ?.addOnFailureListener(requireActivity()) { e ->
-                // No Google Accounts found. Just continue presenting the signed-out UI.
-                displaySignUp()
-                Log.d("btn click", e.localizedMessage!!)
-            }
+        loginViewModel.hasGoogleSignIn.observe(this.viewLifecycleOwner) {
+            if (it == false) return@observe
+            else { this.findNavController().navigate(R.id.action_welcomeFragment_to_mainActivity) }
+            loginViewModel.resetSignInGoogle()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/ngockieubao/orderapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ngockieubao/orderapp/ui/login/LoginViewModel.kt
@@ -1,21 +1,38 @@
 package com.ngockieubao.orderapp.ui.login
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.Application
+import android.content.Context
+import android.content.IntentSender
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
+import com.ngockieubao.orderapp.BuildConfig
 import com.ngockieubao.orderapp.data.Login
 import com.ngockieubao.orderapp.data.User
 import com.ngockieubao.orderapp.util.Constants
 import com.ngockieubao.orderapp.util.TextUtils
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 @Suppress("KotlinConstantConditions")
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
@@ -26,6 +43,13 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private val auth: FirebaseAuth = Firebase.auth
     private val db = Firebase.firestore
 
+    private var _oneTapClient: SignInClient? = null
+    val oneTapClient get() = _oneTapClient
+    private var _signUpRequest: BeginSignInRequest? = null
+    val signUpRequest get() = _signUpRequest
+    private var _signInRequest: BeginSignInRequest? = null
+    val signInRequest get() = _signInRequest
+
     private val _signUp = MutableLiveData<Login?>()
     val signUp: LiveData<Login?>
         get() = _signUp
@@ -33,6 +57,10 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private val _login = MutableLiveData<Login?>()
     val login: LiveData<Login?>
         get() = _login
+
+    private val _hasGoogleSignIn = MutableLiveData<Boolean?>(false)
+    val hasGoogleSignIn: LiveData<Boolean?>
+        get() = _hasGoogleSignIn
 
     fun createAccount(email: String?, passwd: String?) {
         if (!TextUtils.checkNull(email, passwd)) {
@@ -93,13 +121,14 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         _login.value = null
     }
 
-    private fun checkCurrentUser(): FirebaseUser? {
+    fun checkCurrentUser(): FirebaseUser? {
         val user = Firebase.auth.currentUser
-        if (user != null) return user
-        else return null
+        if (user != null) {
+            return user
+        } else return null
     }
 
-    fun addUserData() {
+    private fun addUserData() {
         val user = User(
             checkCurrentUser()?.uid,
             Constants.getUsernameFromEmail(checkCurrentUser()?.email),
@@ -119,6 +148,148 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
                     Log.d(TAG, "addUserData: $exception - failed")
                 }
         }
+    }
+
+    fun initParams(context: Context) {
+        _oneTapClient = Identity.getSignInClient(context)
+        _signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build()
+            )
+            .build()
+        _signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(BuildConfig.WEB_CLIENT_ID)
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build()
+            )
+            .build()
+    }
+
+    fun initOneTapResult(result: ActivityResult, activity: Activity) {
+        try {
+            val credential = oneTapClient?.getSignInCredentialFromIntent(result.data)
+            val idToken = credential?.googleIdToken
+            when {
+                idToken != null -> {
+                    // Got an ID token from Google. Use it to authenticate with your backend.
+                    val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+                    auth.signInWithCredential(firebaseCredential)
+                        .addOnCompleteListener(activity) { task ->
+                            if (task.isSuccessful) {
+                                // Sign in success, update UI with the signed-in user's information
+                                val currentUserId = auth.currentUser?.uid
+                                val msg = "user ${auth.currentUser?.email} signed in"
+                                _hasGoogleSignIn.value = true
+                                viewModelScope.launch { checkUserSignInGoogle(currentUserId) }
+                                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithCredential: failure", task.exception)
+//                                updateUI(null)
+                            }
+                        }
+                }
+                else -> {
+                    // Shouldn't happen.
+                    Log.d("one tap", "No ID token!")
+//                    Snackbar.make(binding.root, "No ID token!", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        } catch (e: ApiException) {
+            when (e.statusCode) {
+                CommonStatusCodes.CANCELED -> {
+                    Log.d("one tap", "One-tap dialog was closed.")
+                    // Don't re-prompt the user.
+//                    Snackbar.make(binding.root, "One-tap dialog was closed.", Snackbar.LENGTH_SHORT).show()
+                }
+                CommonStatusCodes.NETWORK_ERROR -> {
+                    Log.d("one tap", "One-tap encountered a network error.")
+                    // Try again or just ignore.
+//                    Snackbar.make(binding.root, "One-tap encountered a network error.", Snackbar.LENGTH_SHORT).show()
+                }
+                else -> {
+                    Log.d("one tap", "Couldn't get credential from result." + " (${e.localizedMessage})")
+//                    Snackbar.make( binding.root, "Couldn't get credential from result.\" +\n" +
+//                                " (${e.localizedMessage})", Snackbar.LENGTH_SHORT ).show()
+                }
+            }
+        }
+    }
+
+    private suspend fun checkUserSignInGoogle(currentUserId: String?) {
+        val query = db.collection("UserInfo").get().await()
+        if (query.documents.isNotEmpty()) {
+            val listUserId = mutableListOf<String>()
+            val queryToObj = query.toObjects<User>()
+            for (user in queryToObj) {
+                listUserId.add(user.userID!!)
+            }
+            if (!listUserId.contains(currentUserId)) {
+                addUserData()
+                Toast.makeText(context, "addUserData done", Toast.LENGTH_SHORT).show()
+            } else {
+                Log.d(TAG, "initOneTapResult: user existed")
+            }
+        }
+    }
+
+    fun resetSignInGoogle() {
+        _hasGoogleSignIn.value = false
+    }
+
+    fun displaySignIn(
+        activity: Activity,
+        oneTapClient: SignInClient?,
+        signInRequest: BeginSignInRequest?,
+        signUpRequest: BeginSignInRequest?,
+        oneTapResult: ActivityResultLauncher<IntentSenderRequest>
+    ) {
+        oneTapClient?.beginSignIn(signInRequest!!)
+            ?.addOnSuccessListener(activity) { result ->
+                try {
+                    val ib = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                    oneTapResult.launch(ib)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("btn click", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            ?.addOnFailureListener(activity) { e ->
+                // No Google Accounts found. Just continue presenting the signed-out UI.
+                displaySignUp(activity, oneTapClient, signUpRequest, oneTapResult)
+                Log.d("btn click", e.localizedMessage!!)
+            }
+    }
+
+    private fun displaySignUp(
+        activity: Activity,
+        oneTapClient: SignInClient?,
+        signUpRequest: BeginSignInRequest?,
+        oneTapResult: ActivityResultLauncher<IntentSenderRequest>
+    ) {
+        oneTapClient?.beginSignIn(signUpRequest!!)
+            ?.addOnSuccessListener(activity) { result ->
+                try {
+                    val ib = IntentSenderRequest.Builder(result.pendingIntent.intentSender).build()
+                    oneTapResult.launch(ib)
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e("btn click", "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            ?.addOnFailureListener(activity) { e ->
+                // No Google Accounts found. Just continue presenting the signed-out UI.
+                Log.d("btn click", e.localizedMessage!!)
+            }
     }
 
     override fun onCleared() {
